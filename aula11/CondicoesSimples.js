74c17d23d9dec214d3d5b2364609a2437b964af7
Mateus Victor <mateusvictorct@gmail.com>
2024-01-03 17:01:46 -0300

    inserido uma pasta de com arquivos de condições

---
+++ aula11/CondicoesSimples.js
@@ -0,0 +1,8 @@
+var vel = 45
+console.log(`a velocidade do seu carro é ${vel} km/h `)
+
+if (vel > 60){ // condição simples
+    console.log('você ultrapassou a velocidade permitida. Multado')
+}
+
+    console.log('Dirija sempre usando cinto de segurança')
